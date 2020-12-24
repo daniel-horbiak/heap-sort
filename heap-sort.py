4fda758a235ddf76f2cd6390f566997bf175673c
daniel-horbiak <46420905+daniel-horbiak@users.noreply.github.com>
2020-12-25 00:36:51 +0200

    Add files via upload

--- heap-sort.py
+++ heap-sort.py
@@ -1,10 +1,13 @@
 def heap(a):
 	for i in range(len(a)-1, 0, -1):
 		if i % 2 == 0:
-			nParent = int((i-2)/2)
-			print(a[nParent], a[i])
+			indexParent = int((i-2)/2)
+			swap = min(a[indexParent], a[i], a[int((i-1)/2)])
+			a[indexParent], swap = swap, a[indexParent]
+			print(a[indexParent], a[i])
 		else:
-			nParent = int((i-1)/2)
-			print(a[nParent], a[i])
-
+			indexParent = int((i-1)/2)
+			swap = min(a[indexParent], a[i], a[int((i-2)/2)])
+			a[indexParent], swap = swap, a[indexParent]
+			print(a[indexParent], a[i])
 	return a
